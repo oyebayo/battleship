b82190f77552bd528c4bfcbb311834da46d7cd05
Bayo Oyekole <oyebayo@gmail.com>
2023-12-05 12:49:07 +0000

    Update junit reference

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -1,5 +1,5 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
