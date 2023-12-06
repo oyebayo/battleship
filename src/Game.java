e1505b7b67d7d4fe7c4134d414c9805c98149035
Bayo Oyekole <oyebayo@gmail.com>
2023-12-06 07:28:45 +0000

    Clean up the main game class

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.io.File;
