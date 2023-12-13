12f5c78938ab453e7f380c9658f4c019ae01e364
Bayo Oyekole <oyebayo@gmail.com>
2023-12-13 04:28:19 +0100

    improve coverage

--- src/test/java/com/findcomputerstuff/apps/battleship/CellTest.java
+++ src/test/java/com/findcomputerstuff/apps/battleship/CellTest.java
@@ -2,27 +2,44 @@ package com.findcomputerstuff.apps.battleship;
 
 import com.findcomputerstuff.apps.battleship.entities.Cell;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 class CellTest {
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1, 'A', 1, 1, 'A', true",
+            "1, 1, 'A', 1, 1, 'B', true",
+            "1, 1, 'A', 2, 2, 'A', false"
+    })
+    void equalsTest(int row1, int col1, char letter1, int row2, int col2, char letter2, boolean expected) {
+        Cell cell1 = new Cell(row1, col1, letter1);
+        Cell cell2 = new Cell(row2, col2, letter2);
+        if (expected) {
+            assertEquals(cell1, cell2);
+        } else {
+            assertNotEquals(cell1, cell2);
+        }
+    }
     @Test
-    void equalsReturnsTrueForSameCell() {
-        Cell cell1 = new Cell(1, 1, 'A');
-        Cell cell2 = new Cell(1, 1, 'A');
-        assertEquals(cell1, cell2);
+    void equalsReturnsFalseForNullObject() {
+        Cell cell = new Cell(1, 1, 'A');
+        assertNotEquals(null, cell);
     }
 
     @Test
-    void equalsReturnsTrueForSameCell_withDifferentLetter() {
+    void equalsReturnsFalseForNonCellTypeObject() {
+        Cell cell = new Cell(1, 1, 'A');
+        Object nonCellObject = new Object();
+        assertNotEquals(cell, nonCellObject);
+    }
+
+    @Test
+    void hashCodeTest() {
         Cell cell1 = new Cell(1, 1, 'A');
         Cell cell2 = new Cell(1, 1, 'B');
-        assertEquals(cell1, cell2);
-    }
-
-    @Test
-    void equalsReturnsFalseForDifferentCells() {
-        Cell cell1 = new Cell(1, 1, 'A');
-        Cell cell2 = new Cell(2, 2, 'A');
-        assertNotEquals(cell1, cell2);
+        assertEquals(cell1.hashCode(), cell2.hashCode());
     }
 
     @Test
